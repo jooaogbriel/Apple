4162659001a6ca9e928f90148c07870c01f30cc9
João Gonçalves <joao_i0807@outlook.com>
2023-02-28 16:47:56 -0300

    feat: finish dashboard

--- src/components/Dash/index.tsx
+++ src/components/Dash/index.tsx
@@ -16,8 +16,9 @@ export const Dash = () => {
     return (
         <>
             <Container>
+                <div className="main">
                 <div className="textDashboard">
-                    <h1>Loja. O melhor jeito de comprar o que você ama.</h1>
+                    <h1><span>Loja. </span>O melhor jeito de comprar o que você ama.</h1>
                 </div>
                
                 <div className="aside">
@@ -25,28 +26,60 @@ export const Dash = () => {
                         <img className="asideOne-Image" src={iconPeaple} alt="" />
                         <div className="asideOne-Text">
                             <a>Precisa de ajuda para comprar?</a>
-                            <a>Fale com um especialista</a>
+                            <a className="link" >Fale com um especialista</a>
                         </div>
                     </div>
                     <div className="asideOne">
                         <img className="asideOne-Img" src={iconApple} alt="" />
                         <div className="asideOne-Text">
                             <a>Visite uma Apple Store</a>
-                            <a>Procure uma loja perto de você {'>'}</a>
+                            <a className="link" >Procure uma loja perto de você {'>'}</a>
                         </div>
                     </div>
-                    <div className="asideAfter">
-                        <img src={mac} alt="" />
-                        <img src={iphone} alt="" />
-                        <img src={ipad} alt="" />
-                        <img src={watch} alt="" />
-                        <img src={pods} alt="" />
-                        <img src={airtag} alt="" />
-                        <img src={tv} alt="" />
-                        <img src={accesories} alt="" />
                     </div>
+                    </div>
+                    <div className="asideAfter">
+                        <div className="asideAfter-text">
+                            <img className="imageDash" src={mac} alt="" />
+                            <p>Mac</p>
+                        </div>
 
-                </div>
+                        <div className="asideAfter-text">
+                            <img className="imageDash"  src={iphone} alt="" />
+                            <p>iPhone</p>
+                        </div>
+
+                        <div className="asideAfter-text">
+                            <img className="imageDash" src={ipad} alt="" />
+                            <p>iPad</p>
+                        </div>
+
+                        <div className="asideAfter-text">
+                            <img className="imageDash" src={watch} alt="" />
+                            <p>Apple Watch</p>
+                        </div>
+
+                        <div className="asideAfter-text">
+                            <img className="imageDash" src={pods} alt="" />
+                            <p>AirPods</p>
+                        </div>
+
+                        <div className="asideAfter-text">
+                            <img className="imageDash" src={airtag} alt="" />
+                            <p>AirTag</p>
+                        </div>
+
+                        <div className="asideAfter-text">
+                            <img className="imageDash" src={tv} alt="" />
+                            <p>Apple TV 4K</p>
+                        </div>
+
+                       <div className="asideAfter-text">
+                            <img className="imageDash" src={accesories} alt="" />
+                            <p>Acessórios</p>
+                       </div>
+                        
+                    </div>
             </Container>
         </>
     )
--- src/components/Dash/styles.ts
+++ src/components/Dash/styles.ts
@@ -5,44 +5,104 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
+    
 
     h1{
-        font-size: 40px;
+        font-size: 46px;
+        color: #6e6e73;
+        font-weight: 500;
+        letter-spacing: -.003em;
+        line-height: 1.08349;
     }
     .aside{
         display: flex;
         flex-direction: column;
         
     }
+    .main{
+        display: flex;
+        align-items: center;
+
+        
+    }
     .textDashboard{
         font-size: 48px;
-    line-height: 1.08349;
-    font-weight: 600;
-    letter-spacing: -.003em;
-    font-family: SF Pro Display,SF Pro Icons,AOS Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
-    display: inline;
+        line-height: 1.08349;
+        font-weight: 600;
+        letter-spacing: -.003em;
+        font-family: SF Pro Display,SF Pro Icons,AOS Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
+        display: inline;
+        max-width: 640px;
+        padding: 80px 130px 64px 20px;
     }
     .asideOne{
         display: flex;
+        align-items: center;
+        justify-content: center;
         
     }
     .asideOne-Image{
         width: 30px;
+        height: 30px;
+        padding-right: 8px;
     }
     .asideOne-Img{
         width: 30px;
+        height: 30px;
         border-radius: 50%;
+        padding-right: 8px;
     }
     .asideOne-Text{
         display: flex;
         flex-direction: column;
+        padding-bottom: 12px;
        
+    }
+    span{
+        color: black;
+    }
+    a{
+        font-family: SF Pro Text,SF Pro Icons,AOS Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
+        font-weight: 600;
+        font-size: 14px;
+        padding:3px;
+    }
+    .link{
+        color: #06c;
+        cursor: pointer;
+        font-weight: normal;
     }
     .asideAfter{
         display: flex;
         align-items: center;
+        height: 148px;
+        min-width: 136px;
+        padding: 18px 8px 16px;
+        max-width: 640px;
+        padding: 30px 370px 64px 20px;
     }
     img{
         width: 70px;
+    }
+    p{
+        font-size: 14px;
+        color: #1d1d1f;
+        font-weight: 520;
+        font-family: SF Pro Text,SF Pro Icons,AOS Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
+        letter-spacing: -.016em;
+        line-height: 1.42859;
+    }
+    .imageDash{
+        max-height: 78px;
+        height: 130px;
+        width: 120px;
+        padding-bottom: 16px;
+        padding-right: 25px;
+    }
+    .asideAfter-text{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 `
